16a7998796768122b61e66a3024f026e68299c2f
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio.local>
2019-09-23 18:17:30 -0300

    adding option to login or registration in LP

--- src/pages/default/landingPage.vue
+++ src/pages/default/landingPage.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    <h1>Landing page</h1>
+    <h1 class="row justify-center">Landing page</h1>
+    <div class="row justify-center">
+      <img src="../../assets/robo-quasar-logo-full.svg" />
+    </div>
+    <div class="row justify-center">
+      <q-btn
+        color="primary"
+        class="full-width q-mt-md"
+        style="max-width: 400px"
+        :to="{name: 'login'}"
+      >Login</q-btn>
+    </div>
+    <div class="row justify-center q-mt-md">ou</div>
+    <div class="row justify-center">
+      <q-btn
+        color="positive"
+        class="full-width q-mt-md"
+        style="max-width: 400px"
+        :to="{name: 'register'}"
+      >Registrar</q-btn>
+    </div>
   </div>
 </template>
 
