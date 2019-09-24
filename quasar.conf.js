60137b88b28014e84f003587c419e723bd70fc66
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio.local>
2019-09-23 22:53:55 -0300

    add notify

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,7 +48,14 @@ module.exports = function (ctx) {
       directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ],
+      config: {
+        notify: {
+          /* Notify defaults */
+        }
+      }
     },
 
     // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,10 +1,45 @@
 <template>
-  <div>Home</div>
+  <div>
+    Home
+    <div class="q-pa-md">
+      <q-btn color="primary" @click="showNotif" label="Mostrar notificação de exemplo"></q-btn>
+    </div>
+  </div>
 </template>
 
 <script>
+import { Notify } from "quasar";
 export default {
-  name: "home"
+  name: "home",
+  methods: {
+    showNotif() {
+      this.$q.notify({
+        position: "bottom",
+        message: "Aceita as políticas de cookies?",
+        color: "gray",
+        timeout: 999999,
+        avatar: "https://cdn.quasar.dev/img/boy-avatar.png",
+        actions: [
+          {
+            icon: "check_box",
+            label: "Reply",
+            color: "white",
+            handler: () => {
+              /* console.log('wooow') */
+            }
+          },
+          {
+            icon: "close",
+            label: "Dismiss",
+            color: "yellow",
+            handler: () => {
+              /* console.log('wooow') */
+            }
+          }
+        ]
+      });
+    }
+  }
 };
 </script>
 
